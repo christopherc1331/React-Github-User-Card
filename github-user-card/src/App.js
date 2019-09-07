1f10dbbc2cfd2d768d26fc8331132a6861750435
Chris Cruz <christopherc1331@gmail.com>
2019-09-06 19:19:25 -0700

    Fetched data & added to state

--- github-user-card/src/App.js
+++ github-user-card/src/App.js
@@ -1,26 +1,22 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    githubUser: []
+  };
+
+  componentDidMount() {
+    fetch("https://api.github.com/users/christopherc1331")
+      .then(res => res.json())
+      .then(res2 => console.log("fetch request data: ", res2))
+      .then(userInfo => this.setState(userInfo))
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return <div className="App"></div>;
+  }
 }
 
 export default App;
